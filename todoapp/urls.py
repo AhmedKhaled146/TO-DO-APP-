13b4d4dd2bed47e83fc27208f194f4f8b54b5d32
AhmedKhaled146 <a7med74yaso@gmail.com>
2023-10-18 10:00:54 +0300

    replace class to function

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,10 +1,20 @@
+# from django.urls import path
+# from .views import *
+
+# urlpatterns = [
+#     path('', TodoList.as_view()),
+#     path('<int:pk>/', TodoDetail.as_view()),
+#     path('delete/<int:pk>', DeleteTodo.as_view()),
+#     path('create/', CreateTodo.as_view()),
+# ]
+
+
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('', TodoList.as_view()),
-    path('<int:pk>/', TodoDetail.as_view()),
-    path('delete/<int:pk>', DeleteTodo.as_view()),
-    path('create/', CreateTodo.as_view()),
-
+    path('todos/', views.todo_list, name='todo-list'),
+    path('todos/<int:pk>/', views.todo_detail, name='todo-detail'),
+    path('create-todo/', views.create_todo, name='create-todo'),
+    path('delete-todo/<int:pk>/', views.delete_todo, name='delete-todo'),
 ]
